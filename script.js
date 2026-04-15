04b9fba0fb7f4ff42b8a54f711773b8e33ef32b6
AmandaTi <151983009+Amandaad@users.noreply.github.com>
2026-04-15 17:50:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+function abrirChat(){
+
+let mensagem = prompt("💇‍♀️ Studio Amanda\n\n1- Agendar\n2- Serviços\n3- Horários");
+
+if(mensagem == 1){
+alert("Qual seu nome?");
+}
+
+else if(mensagem == 2){
+alert("Escova, Selagem, Mechas, Cílios...");
+}
+
+else if(mensagem == 3){
+alert("Funcionamos das 8h às 18h");
+}
+
+}
